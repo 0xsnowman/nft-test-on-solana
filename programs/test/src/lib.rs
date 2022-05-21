860751e6c65065bf083718f0e26ad01606444263
0xsnowman <eth.snowman@gmail.com>
2022-05-21 20:41:14 +0800

    nft mint

--- programs/test/src/lib.rs
+++ programs/test/src/lib.rs
@@ -3,13 +3,99 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod test {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+pub fn mint_nft(
+        ctx: Context<MintNFT>,
+        creator_key: Pubkey,
+        uri: String,
+        title: String,
+    ) -> Result<()> {
+        msg!("Initializing Mint NFT");
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.payer.to_account_info(),
+        };
+ msg!("CPI Accounts Assigned");
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        msg!("CPI Program Assigned");
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        msg!("CPI Context Assigned");
+        token::mint_to(cpi_ctx, 1)?;
+        msg!("Token Minted !!!");
+        let account_info = vec![
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        msg!("Account Info Assigned");
+        let creator = vec![
+            mpl_token_metadata::state::Creator {
+                address: creator_key,
+                verified: false,
+                share: 100,
+            },
+            mpl_token_metadata::state::Creator {
+                address: ctx.accounts.mint_authority.key(),
+                verified: false,
+                share: 0,
+            },
+        ];
+        msg!("Creator Assigned");
+        let symbol = std::string::ToString::to_string("symb");
+        invoke(
+            &create_metadata_accounts_v2(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.payer.key(),
+                title,
+                symbol,
+                uri,
+                Some(creator),
+                1,
+                true,
+                false,
+                None,
+                None,
+            ),
+            account_info.as_slice(),
+        )?;
+        msg!("Metadata Account Created !!!");
+        let master_edition_infos = vec![
+            ctx.accounts.master_edition.to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.metadata.to_account_info(),
+            ctx.accounts.token_metadata_program.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+        ];
+        msg!("Master Edition Account Infos Assigned");
+        invoke(
+            &create_master_edition_v3(
+                ctx.accounts.token_metadata_program.key(),
+                ctx.accounts.master_edition.key(),
+                ctx.accounts.mint.key(),
+                ctx.accounts.payer.key(),
+                ctx.accounts.mint_authority.key(),
+                ctx.accounts.metadata.key(),
+                ctx.accounts.payer.key(),
+                Some(0),
+            ),
+            master_edition_infos.as_slice(),
+        )?;
+        msg!("Master Edition Nft Minted !!!");
         Ok(())
     }
-}
 
 #[derive(Accounts)]
 pub struct Initialize {}
