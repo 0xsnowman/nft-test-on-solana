238e699be39046a22b356160b0130d7c7dbc52d5
0xsnowman <eth.snowman@gmail.com>
2022-05-22 09:03:47 +0800

    added program id

--- programs/test/src/lib.rs
+++ programs/test/src/lib.rs
@@ -1,6 +1,5 @@
 // use anchor_lang::prelude::*;
 
-// declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 // #[program]
 // pub mod test {
@@ -19,6 +18,8 @@ use anchor_lang::solana_program::program::invoke;
 use anchor_spl::token;
 use anchor_spl::token::{MintTo, Token};
 use mpl_token_metadata::instruction::{create_master_edition_v3, create_metadata_accounts_v2};
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod test {
